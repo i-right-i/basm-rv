fbecd4faf74d565e9a6bc17f9dfd32d0e494506c
i-right-i <olmstead.ben@gmail.com>
2022-04-24 08:50:24 +0000

    Update example.s

--- example.s
+++ example.s
@@ -8,7 +8,7 @@
 						
 						
 ADD 	x0,x0,x0		# For instance this NOP instructions address is equal to :Start:
-ADDI	x1,x0,@constant # Add's Immedant value of constant to Register x1
+ADDI	x1,x0,@constant 	# Add's Immedant value of constant to Register x1
 BEQ		x0,x1,>Start	# If x1 = zero jump to :Start:  
 ADD 	x1,x2,x3		# x1 = x2 + x3 
 
